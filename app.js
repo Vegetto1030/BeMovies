d6c1c9711d5a6998c8e578456009498b57f0e24f
Vegetto1030 <adil.idrissi.hr@gmail.com>
2024-06-21 15:06:15 +0200

    Little changes

--- app.js
+++ app.js
@@ -5,31 +5,69 @@ const swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 19,
     navigation: {
-      nextEl: ".button-next",
-      prevEl: ".button-prev",
-      clickable: true,
+        nextEl: ".button-next",
+        prevEl: ".button-prev",
+        clickable: true,
     },
-  });
-  
-  const swiper1 = new Swiper(".mySwiper1", {
+});
+const swiper1 = new Swiper(".mySwiper1", {
     slidesPerView: 4,
     spaceBetween: 19,
     navigation: {
-      nextEl: ".button-next1",
-      prevEl: ".button-prev1",
-      clickable: true,
+        nextEl: ".button-nextL",
+        prevEl: ".button-prevL",
+        clickable: true,
     },
-  });
-  
-  const swiper2 = new Swiper(".mySwiper2", {
+});
+const swiper2 = new Swiper(".mySwiper2", {
     slidesPerView: 4,
     spaceBetween: 19,
     navigation: {
-      nextEl: ".button-next2",
-      prevEl: ".button-prev2",
-      clickable: true,
+        nextEl: ".button-nextG",
+        prevEl: ".button-prevG",
+        clickable: true,
     },
-  });
-  
- 
-  
+});
+
+//Login
+
+const login = document.querySelectorAll(".signin")
+const modal = document.querySelector(".modal")
+const signupbtn = document.querySelector(".signup")
+const loginbtn = document.querySelector(".login")
+const signupBox = document.querySelector(".signup-box")
+const loginBox = document.querySelector(".login-box")
+const form = document.querySelector(".form-section")
+login.forEach((login) => {
+    login.addEventListener("click", () => {
+        modal.style.display="block"
+        signupbtn.addEventListener("click", ()=> {
+            signupBox.style.display = "flex"
+            loginBox.style.display= "none"
+            signupbtn.style.background= "#C00"
+            signupbtn.style.border = "none"
+            loginbtn.style.background = "black"
+            loginbtn.style.border = "1px solid white"
+        });
+    loginbtn.addEventListener("click", ()=> {
+        signupBox.style.display = "none"
+        loginBox.style.display= "flex"
+        signupbtn.style.background= "black"
+        signupbtn.style.border = "1px solid white"
+        loginbtn.style.background = "#C00"
+        loginbtn.style.border = "none"
+    });
+    
+    document.addEventListener("click", (e) => {
+        if (e.target.matches(".close")) {
+            console.log("click");
+            modal.style.display = "none";
+        }
+    })
+    })
+})
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    modal.style.display = "none";
+})
+
